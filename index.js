d8d427ba0a8ecde90d1132b9e5ec82f084b3409e
Deepaksor <137423091+Deepaksor@users.noreply.github.com>
2023-07-24 14:46:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const item = document.querySelector("#item");
+const todobox = document.querySelector("#todobox");
+
+item.addEventListener(
+    "keyup",
+    function (event) {
+        if (event.key == "Enter") {
+            addtodo(this.value)
+            this.value = ""
+        }
+    }
+)
+const addtodo = (item) => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+    ${item}
+    <i class="fas fa-times"></i>
+    `;
+
+    listItem.addEventListener(
+        "click",
+        function(){
+            this.classList.toggle("done");
+        }
+    )
+    listItem.querySelector("i").addEventListener(
+        "click",
+        function(){
+            listItem.remove();
+        }
+    )
+
+    todobox.appendChild(listItem)
+}
